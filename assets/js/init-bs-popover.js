a89a1d1b71e9f3b010b6be3294132fef8c7e0504
Matt Clark <matthew.clark@undp.org>
2016-02-03 17:28:21 +1100

    Removed files created during merge with GITP theme repo

--- assets/js/init-bs-popover.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-    Drupal.behaviors.initbspopover = {
-        attach: function (context, settings) {
-            $('[data-toggle="popover"]').popover();
-        }
-    };
-}) (jQuery);
